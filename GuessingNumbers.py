9dda26b3618119e3627818ae3ae1c3a3b11a8647
g4829 <60137097+g4829@users.noreply.github.com>
2020-11-11 16:19:27 +0530

    Add files via upload

---
+++ GuessingNumbers.py
@@ -0,0 +1,11 @@
+name1=input("Guess a number 1-9")
+number1=("A number that is divisible by 2")
+text1="CONGRATUALATIONS"
+text2="TRY AGAIN"
+for i  in name1:
+    number2=4
+    if(input==number2):
+        print(text1)
+    else:
+         print(text2)
+         print(number1)
